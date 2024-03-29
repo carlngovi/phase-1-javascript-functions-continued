ed61d3b95bf049e736d03717794c32a08875a6fb
Carl Ngovi <carl.ngovi@student.moringaschool.com>
2024-03-29 15:54:38 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun (activity="roller-skate"){
+    if (activity==="roller-skate"){
+     return "This Saturday, I want to roller-skate!"};
+    if (activity!=="roller-skate"){
+        return "This Saturday, I want to bathe my dog!"};
+};
+function mondayWork (activity="go to the office"){
+    if (activity==="go to the office"){
+        return "This Monday, I will go to the office."};
+    if (activity!=="go to the office"){
+        return "This Monday, I will work from home."};
+};
+let wrapAdjective = function(style){
+    if (style==="*"){
+        return function(adjective="a hard woerker") {
+            return `You are ${style}${adjective}${style}!`}
+    }
+    if (style==="||"){
+        return function(adjective="a dedicated programmer"){
+            return "You are " +style+adjective+style+ "!"}
+    }
+  }
